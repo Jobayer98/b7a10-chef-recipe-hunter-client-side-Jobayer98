edb0e232f9437078ec23d6362572ee58a4e6880a
Jobayer <jobayerrahman0220@gmail.com>
2023-05-03 10:18:38 +0600

    add banner section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import bannerImg from "../assets/banner.jpg";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url(${bannerImg})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-30"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
